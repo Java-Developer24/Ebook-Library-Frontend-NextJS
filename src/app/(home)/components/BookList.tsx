c11357fc498f7690ad70c8ea47e025ba88e8dbb5
Developer@2446 <balu2446madhu@gmail.com>
2024-10-01 01:08:30 +0530

    add suspense

--- src/app/(home)/components/BookList.tsx
+++ src/app/(home)/components/BookList.tsx
@@ -1,12 +1,39 @@
-import { Book } from '@/types'
+
 import React from 'react'
 import BookCard from './BookCard'
+import { Book } from '@/types';
 
-const BookList = ({books}:{books:Book[]}) => {
+const BookList = async() => {
+  //data fetching
+
+
+
+///Getting data from API using Axios function
+// const response = await axios.get("http://localhost:4000/api/users/books");
+
+//     const books = response.data; // Axios already parses the JSON response
+//     console.log("Fetched books from API:", books);
+
+
+    //Getting data from API using fecth function
+    const response = await fetch(`${process.env.BACKEND_URL}/books`, {
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+    });
+    
+    if (!response.ok) {
+      throw new Error("An error occurred while fetching the books");
+    }
+    
+    const books = await response.json();
+    console.log("Fetched books from API:", books);
+    
   return (
     <div className='grid grid-cols-1 gap-8 md:grid-cols-3 max-w-7xl mx-auto mb-10'>
       
-     {books.map((book)=>(
+     {books.map((book:Book)=>(
       
       <BookCard key={book._id} book={book}/>
      ))}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,34 +1,11 @@
 import Banner from "@/app/(home)/components/Banner";
 import BookList from "@/app/(home)/components/BookList";
+import Loading from "@/components/Loading";
+import { Suspense } from "react";
 // import axios from "axios";
 
 export default async function Home() {
-//data fetching
 
-
-
-///Getting data from API using Axios function
-// const response = await axios.get("http://localhost:4000/api/users/books");
-
-//     const books = response.data; // Axios already parses the JSON response
-//     console.log("Fetched books from API:", books);
-
-
-    //Getting data from API using fecth function
-    const response = await fetch(`${process.env.BACKEND_URL}/books`, {
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-      },
-    });
-    
-    if (!response.ok) {
-      throw new Error("An error occurred while fetching the books");
-    }
-    
-    const books = await response.json();
-    console.log("Fetched books from API:", books);
-    
 
 
 
@@ -36,7 +13,11 @@ export default async function Home() {
 
 
     <Banner/>
-    <BookList books={books}/>
+    <Suspense fallback={<Loading/>}>
+
+    <BookList />
+    </Suspense>
+    
     
     
     </>
