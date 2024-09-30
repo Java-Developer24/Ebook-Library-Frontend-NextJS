57ead1b83b16b1f491e5eb8c0b08bca4614e0cf7
Developer@2446 <balu2446madhu@gmail.com>
2024-10-01 00:33:14 +0530

    add footer page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="border-t p-5 text-center">
+            <span>© 2024 Coder&#39;s Book All rights reserved.</span>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
