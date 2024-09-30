29a1215f7c7d4f9746453c15dedd4875f63d2c67
Developer@2446 <balu2446madhu@gmail.com>
2024-09-30 23:46:48 +0530

    create book card

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,23 +1,34 @@
 import Banner from "@/app/(home)/components/Banner";
 import BookList from "@/app/(home)/components/BookList";
+// import axios from "axios";
 
 export default async function Home() {
 //data fetching
-const response = await fetch(`${process.env.BACKEND_URL}/books`);
 
-//const response =await fetch("http://localhost:4000/api/users/books")
 
-if(!response.ok){
-  throw new Error("An error occured while fetching the books");
-}
 
-const books=await response.json();
-console.log("Fetched books from API:", books);
-// const response = await fetch(`${process.env.BACKEND_URL}/books`);
-// const data = await response.text(); // Get the raw text
-// console.log("Raw response:", data); // Check the raw response
-// const books = JSON.parse(data); // Parse it into JSON
-// console.log("Parsed books:", books);
+///Getting data from API using Axios function
+// const response = await axios.get("http://localhost:4000/api/users/books");
+
+//     const books = response.data; // Axios already parses the JSON response
+//     console.log("Fetched books from API:", books);
+
+
+    //Getting data from API using fecth function
+    const response = await fetch(`${process.env.BACKEND_URL}/books`, {
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+    });
+    
+    if (!response.ok) {
+      throw new Error("An error occurred while fetching the books");
+    }
+    
+    const books = await response.json();
+    console.log("Fetched books from API:", books);
+    
 
 
 
